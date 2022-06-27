1228708aa71b9f66279b8e6647c7b9cfb801f50d
its-Danial <itsmalikdanial@gmail.com>
2022-06-27 18:08:10 +0800

    set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
 
 export default function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </Layout>
   );
 }
